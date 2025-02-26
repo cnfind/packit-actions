6618d105277f840ed97309560139e85be30a149d
cnfind <cnfind@qq.com>
2025-02-27 01:35:58 +0800

    archived

---
+++ files/rk3568/watermelon-pi/mkbootscr.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/mkimage -C none -A arm -T script -n 'flatmax load script' -d ../../bootfiles/rockchip/rk3568/watermelon-pi/boot.cmd ../../bootfiles/rockchip/rk3568/watermelon-pi/boot.scr
